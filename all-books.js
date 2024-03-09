55ad69f2554f34608b5fc09e774ad486011373a4
Miguel Jimenez <migueljimenezaquino@gmail.com>
2024-03-09 15:34:32 -0400

    [ADD] books

--- all-books.js
+++ all-books.js
@@ -3,10 +3,28 @@ document.addEventListener("DOMContentLoaded", async function () {
 
    if(container_books){
     const response = await fetch("https://wordsearch.onbook.es/books.json");
-    const data = await response.json();
-     if(data){
-
+    const books = await response.json();
+     for(const book in books){
         console.log(data)
+        const article = document.createElement("article")
+            article.innerHTML =`
+            <article class="book">
+        <a href="${article.link}">
+          <img
+            src="${article.image}"
+            alt="${article.title}"
+          />
+        </a>
+        <h3>
+          <a href="${article.link}"
+            >${article.title}: ${article.sub_title}</a
+          >
+        </h3>
+      </article>
+
+            `
+        container_books.appendChild(article)
+      
      }
    }
   });
